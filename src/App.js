a5049b77c19ff2441315616db5bae4161b5ee065
benderang26 <benderang26@gmail.com>
2023-07-09 16:14:57 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
+ const [spendingItems, setSpendingItems] = useState('')
+
+  useEffect(()=> {
+    retrieveSpendingItems()
+  }, [])
+
+ const retrieveSpendingItems = async () => {
+    try {
+      const response = await fetch('http://localhost:5001/api/allItems')
+      const data = await response.json()
+
+      console.log(data)
+
+      setSpendingItems(data)
+
+    } catch(err) {
+      console.log(err.message)
+    }
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Your Spending Items</h1>
+      <div className="spendingItems">
+        {spendingItems.length > 0 ? spendingItems.map(spendingItem => (
+            <div className={
+              "spendingItem" + (spendingItem.complete ? " is-complete" : "")
+            } key={spendingItem._id}>
+              <div className="checkbox"></div>
+
+              <div className="text">{spendingItem.title}</div>
+              <div className="text">{spendingItem.description}</div>
+              <div className="text">{spendingItem.amount}</div>
+              <div className="text">{spendingItem.spendingDate}</div>
+
+              <div className="delete-spendingItem">x</div>
+            </div>
+          )) : (
+            <p>You currently have no tasks</p>
+          )}
+      </div>
     </div>
   );
 }
